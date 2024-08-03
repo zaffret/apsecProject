0ecc962ed7a6421d62349e7fc0d480de154b5308
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-03 22:25:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const express = require("express");
 require("dotenv").config();
 
-const userRoutes = require("./public/routes/userRoutes");
-const memberRoutes = require("./public/routes/memberRoutes");
-const creatorRoutes = require("./public/routes/creatorRoutes");
-const adminRoutes = require("./public/routes/adminRoutes");
+const userRoutes = require(".userRoutes");
+const memberRoutes = require(".memberRoutes");
+const creatorRoutes = require(".creatorRoutes");
+const adminRoutes = require(".adminRoutes");
 
 const cors = require("cors");
 
