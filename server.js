107205178d0f3f7167f8fd1a5abae868c3717506
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-03 23:10:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const userRoutes = require("./routes/userRoutes");
