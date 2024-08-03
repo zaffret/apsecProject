3891f7cc7cbfc0151ddfc3d8797762eba6aa2249
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-03 16:46:39 +0800

    Add files via upload

---
+++ nothing.js
@@ -0,0 +1,2 @@
+const lol = 5 > 2 ? 1 : 2;
+console.log(lol);
---
+++ server.js
@@ -0,0 +1,56 @@
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const mongoose = require("mongoose");
+const express = require("express");
+require("dotenv").config();
+
+const userRoutes = require("./routes/userRoutes");
+const memberRoutes = require("./routes/memberRoutes");
+const creatorRoutes = require("./routes/creatorRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+const cors = require("cors");
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.DB_CONNECT).then(() => {
+  console.log("MongoDB connected...");
+});
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "https://127.0.0.1:5000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("", userRoutes);
+app.use("", memberRoutes);
+app.use("", creatorRoutes);
+app.use("", adminRoutes);
+
+// const sslOptions = {
+//   key: fs.readFileSync(path.join(__dirname, "key.pem")),
+//   cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
+// };
+
+// https.createServer(sslOptions, app).listen(PORT, () => {
+//   console.log(`HTTPS server running on port ${PORT}`);
+//   console.log(
+//     `Click here to access  Register Page: https://localhost:${PORT}/html/register.html`
+//   );
+// });
+
+app.listen(PORT, () => {
+  console.log(`HTTP server running on port ${PORT}`);
+  console.log(
+    `Click here to access  Register Page: http://localhost:${PORT}/html/register.html`
+  );
+});
