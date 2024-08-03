bdd055916008e147c364a1f3cafe2b620e7dccde
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-03 20:51:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.DB_CONNECT).then(() => {
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = 5000;
 const corsOptions = {
   origin: "https://127.0.0.1:5000",
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -25,7 +25,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public/html')));
+// app.use(express.static(path.join(__dirname, 'public/html')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("", userRoutes);
