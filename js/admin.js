39f99b3403ed935e9704df92116a31ea4a9bdd06
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-05 00:54:28 +0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
   logout.addEventListener("click", logoutUser);
 });
 
-setInterval(checkTokenExpiry, 60000);
+setInterval(checkTokenExpiry, 1000);
 
 initializePage().then((videos) => {
   displayVideos(videos);
