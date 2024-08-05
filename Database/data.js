d74497162c0827f7c9e6d52b0ba3497cb169b8b8
zaffret <92503732+zaffret@users.noreply.github.com>
2024-08-05 13:50:54 +0800

    Update data.js

--- Database/data.js
+++ Database/data.js
@@ -37,6 +37,10 @@ const UserSchema = new Schema(
       type: Date,
       default: null,
     },
+    attemptResetAfter {
+      type: Date,
+      default: null,
+    },
   },
   { timestamps: true }
 );
